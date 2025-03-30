f9d79586dca3ff09ee37d63ad2036f6c8e7c7cbc
Hammad Ahmed <shammad.c5@hotmail.com>
2025-03-30 12:32:43 +0500

    add a permanent project for demo purposes

--- components/App.vue
+++ components/App.vue
@@ -91,9 +91,10 @@
 
     const tasks = tasksReq.data;
     const projects = projectsReq.data;
+    projects.value.push({title: 'Inbox', id: Math.random().toString(36).slice(2), tasks: []})
 
     projects.value.forEach(project => project.tasks = tasks.value.filter(task => task.project._id === project.id));
-    
+
     let selectedProject = ref(projects.value[0].id);
 
     const title = useAppConfig().title;
